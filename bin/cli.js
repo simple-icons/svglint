simple-icons/svglint

commit 1e4eb075c050896486e58d377dd4ed7425087bb2

fix: support ESM and CJS config files

bin/cli.js
@@ -10,13 +10,11 @@ import SVGLint from "../src/svglint.js";
 // @ts-ignore
 import config from "../src/cli/config.js";
 import meow from "meow";
-import { cosmiconfig } from "cosmiconfig";
 import chalk from "chalk";
 import glob from "glob";
 
 const GUI = new gui();
 const { getConfigurationFile } = config;
-const configExplorer = cosmiconfig("svglint");
 
 const logger = Logger("");
 // Pretty logs all errors, then exits
@@ -64,7 +62,11 @@ process.on("exit", () => {
     try {
         const configFile = await getConfigurationFile(cli.flags.config);
         if (configFile) {
-            configObj = await configExplorer.load(configFile);
+            try {
+                configObj = await import(configFile);
+            } catch(_) {
+                configObj = require(configFile);
+            }
         } else {
             logger.debug("No configuration file found")
             if (cli.flags.config) {

commit b63a829d48033ebc9794a12cd657db500e74046f

refactor: simplify dynamic imports
Change from the approach in 033d7db98348fab795cff983c9ad5ea6e060990c
and replace the try-catch technique of importing by just using
`await import`. From some local testing it seems only that one is ever
used, making the catch branch effectively dead code.

Additionally, this fixes a bug introduced in
1e4eb075c050896486e58d377dd4ed7425087bb2 where CJS config files were
imported but ignored since CJS is always `import`ed on the default
property.

bin/cli.js
@@ -62,11 +62,8 @@ process.on("exit", () => {
     try {
         const configFile = await getConfigurationFile(cli.flags.config);
         if (configFile) {
-            try {
-                configObj = await import(configFile);
-            } catch(_) {
-                configObj = require(configFile);
-            }
+            const module = await import(configFile);
+            configObj = module.default;
         } else {
             logger.debug("No configuration file found")
             if (cli.flags.config) {

src/lib/rule-loader.js
@@ -29,11 +29,7 @@ async function ruleLoader(ruleName, dir="../rules") {
     const importPath = isExternal
         ? "svglint-plugin-" + ruleName
         : path.join(dir, fileName);
-    try {
-        const module = await import(importPath);
-        return module.default;
-    } catch(_) {
-        return require(importPath);
-    }
+    const module = await import(importPath);
+    return module.default;
 }
 export default ruleLoader;